570a5800a1f0424e42e10479d66d8af3aeedcf98
Abhijeet Rao <abhijeetrao7800@gmail.com>
2026-02-23 22:00:48 +0530

    Add routing and pages for Home, Login, Register, and PageNotFound; include Axios for API calls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/register' element = {<Register/>}/>
+      <Route path = '/login' element = {<Login/>}/>
+      <Route path = '*' element = {<PageNotFound/>}/>
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function Home() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [todo, setTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(()=>{
+    const fetchTodo = async ()=>{
+      try {
+        setLoading(true)
+        const res = await axios.get('http://localhost:5000/api/todo')
+        console.log(res.data);
+        setTodo(res.data);
+      } catch (error:String) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+      }
+    }
+  },[])
+  
+  return (
+
+    <div>Home</div>
+  )
+}
+
+export default Home
